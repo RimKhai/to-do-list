008e5fc6e038a80605182c350dd0afad1041e599
Rim Khairullin <44344943+RimKhai@users.noreply.github.com>
2024-01-19 23:43:22 +0300

    add Input

---
+++ src/ui-kit/Input.vue
@@ -0,0 +1,25 @@
+<script setup>
+    const props = defineProps(['styles', 'placeholder', 'type'])
+</script>
+
+<template>
+    <input
+        :class="'input ' + styles"
+        :placeholder="placeholder"
+        :type="type" />
+</template>
+
+<style lang="scss" scoped>
+    .input {
+        border-radius: $border-medium;
+        box-shadow: 0 0 10px $primary-color;
+        height: 50px;
+        width: 500px;
+        padding: 0 20px;
+        transition-duration: 1000ms;
+        &:focus {
+            outline: 0;
+            box-shadow: 0 0 10px $accent-color;
+        }
+    }
+</style>
