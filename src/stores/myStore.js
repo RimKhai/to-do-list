05d41aa472b6da37a374126a3d23f0f073f26b3e
Rim Khairullin <44344943+RimKhai@users.noreply.github.com>
2024-01-19 23:44:05 +0300

    add Store

--- src/stores/myStore.js
+++ src/stores/myStore.js
@@ -1,12 +1,29 @@
-import { defineStore } from "pinia"
-
+import { defineStore } from 'pinia'
 
 export const useMyStore = defineStore('myStore', {
     state: () => ({
-        data: []
+        list: [
+            {
+                id: 1,
+                title: 'Пососать хуй',
+            },
+            {
+                id: 2,
+                title: 'Пососать пизду',
+            },
+            {
+                id: 3,
+                title: 'Пососать жопу',
+            },
+        ],
     }),
 
     actions: {
-        
-    }
+        addDo(title) {
+            this.list = [...this.list, {
+                id: Date.now(),
+                title: title
+            }]
+        }
+    },
 })
