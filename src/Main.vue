7f43f8f1b9442f929fca94db3d7bc2f2cdd81464
Rim Khairullin <44344943+RimKhai@users.noreply.github.com>
2024-01-19 23:45:10 +0300

    add Button and does

--- src/Main.vue
+++ src/Main.vue
@@ -1,7 +1,59 @@
-<script setup></script>
+<script setup>
+    import { ref } from 'vue'
+    import Input from './ui-kit/Input.vue'
+    import { useMyStore } from './stores/myStore'
+
+    const myStore = useMyStore()
+
+    const data = ref('')
+    const changeHandler = (event) => {
+        data.value = event.target.value
+    }
+</script>
 
 <template>
-
+    <main class="flex flex-col justify-center items-center mt-32 space-y-8">
+        <div class="flex space-x-4">
+            <Input
+                placeholder="Введите говно"
+                type="password"
+                v-on:keydown.enter="myStore.addDo(data)"
+                @input="changeHandler" />
+            <button
+                @click="myStore.addDo(data)"
+                class="btn">
+                Жми Давай!
+            </button>
+        </div>
+        <div
+            v-for="item in myStore.list"
+            class="element text-center"
+            :key="item.id">
+            {{ item.title }}
+        </div>
+    </main>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+    .btn {
+        height: 50px;
+        padding: 0 20px;
+        background-color: $secondary-color;
+        border-radius: $border-medium;
+        transition-duration: 1000ms;
+        &:hover {
+            background-color: $accent-color;
+            box-shadow: 0 0 7px $secondary-color;
+        }
+    }
+    .element {
+        padding: 5px 15px;
+        background-color: $secondary-color;
+        border-radius: $border-small;
+        width: 400px;
+        &:hover {
+            background-color: $accent-color;
+            box-shadow: 0 0 7px $secondary-color;
+        }
+    }
+</style>
